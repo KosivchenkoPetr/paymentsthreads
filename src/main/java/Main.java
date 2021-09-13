3765a29017da308db6693d83f363a2ba8cdd7ddb
KosivchenkoPetr <nightdemon@mail.ru>
2021-09-13 14:18:01 +0300

    main code (#2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,86 +1,108 @@
+import java.io.*;
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static HashMap<String,Integer> balance = new HashMap<>();
+
+    public static HashMap<String, Integer> balance = new HashMap<>();
+    public static HashMap<String, BigDecimal> usdCurrency = new HashMap<String, BigDecimal>();
+    public static Thread threadShowBalance;
 
     public static void main(String[] args) {
-        String accountOperation = "";
-        String[] subStr;
 
-        //new ShowBalance("ShowBalance").start();
+        getUsdCurrency(); //fill HashMap usdCurrency
 
-        Runnable showBalance = () -> {
-          do {
-                try {
-                    Main.balance.forEach((k, v) -> {
-                        if (v != 0) {
-                            System.out.format("Остаток в валюте: %s, сумма: %d%n", k, v);
-                        }
-                    });
-                    Thread.sleep(7000);
-                } catch (InterruptedException e) {
-                    //System.out.println("Thread has been interrupted");
-                }
+        readStartBalance(); //reading start balance from txt file
 
-            } while (true);
+        startSowBalanceThread(); //start thread with periodical balance show
 
-        };
+        consoleReading(); //read data from console
 
-        Thread threadShowBalance = new Thread(showBalance);
-        threadShowBalance.start();
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        while((accountOperation = scanner.nextLine()) != "stop"){
-            threadShowBalance.interrupt();
-            if(accountOperation.equals("stop")){
-                threadShowBalance.interrupt();
-                break;
-            }else{
-                String delimeter = " "; // Разделитель
-                subStr = accountOperation.split(delimeter);
 
-                // +++ Проверки на корректность
-                boolean corr = true;
-                int value = 0;
-                if (subStr.length!=2){
-                    System.out.println("В строке должно быть два слова");
-                    corr = false;
-                 }
-
-                try {
-                    value = Integer.parseInt(subStr[1]);
-                }catch (NumberFormatException o) {
-                    System.out.println("введена некорректная сумма");
-                    corr = false;
-                }
-                // --- Проверки на корректность
-
-                if (corr) {
-                    //добавление значения
-                    String key = subStr[0];
-                    //int value = Integer.parseInt(subStr[1]);
-                    addBalance(key, value);
-                }
+    public static void addBalance(String str) {
+        String[] strArray = str.split(" ");
+        boolean corr = true;
+        int value = 0;
+        if (strArray.length != 2) {
+            System.out.println("В строке " + str + " должно быть два слова");
+            corr = false;
+        }
+        try {
+            value = Integer.parseInt(strArray[1]);
+        } catch (NumberFormatException o) {
+            System.out.println("В строке " + str + " содержится некорректная сумма");
+            corr = false;
+        } catch (ArrayIndexOutOfBoundsException o) {
+            System.out.println("В строке " + str + " должно быть два слова");
+            corr = false;
+        }
+        String key = strArray[0];
+        if (corr) {
+            if (!balance.containsKey(key)) {
+                balance.put(key, value);
+            } else {
+                balance.put(key, balance.get(key) + value);
             }
         }
-        threadShowBalance.interrupt();
     }
 
-    public static void showBalance(){
-        balance.forEach((k, v) -> {
-          if (v!=0) {
-              System.out.format("Остаток в валюте: %s, сумма: %d%n", k, v);
-          }
-        });
+    public static void readStartBalance() {
+        System.out.println("Enter file path with start balance");
+        System.out.println("Enter \"0\" to skip file loading");
+        System.out.println("Enter \"1\" to load test file");
+
+        Scanner in = new Scanner(System.in);
+        String filename = in.nextLine();
+
+        if (filename.equals("0")) //nothing to do
+            System.out.println("File loading skipped");
+        else {
+            if (filename.equals("1")) {
+                filename = "startBalance.txt";
+            }
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+                for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                    //String[] subStr = line.split(" ");
+                    //addBalance(subStr[0], Integer.parseInt(subStr[1]));
+                    addBalance(line);
+                }
+            } catch (FileNotFoundException f) {
+                System.out.println(filename + " does not exist, file not loaded");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ;
     }
 
-    public static void addBalance(String key, Integer value) {
-        if (!balance.containsKey(key)) {
-            balance.put(key, value);
-        } else {
-            balance.put(key, balance.get(key) + value);
+    public static void startSowBalanceThread() {
+        ShowBalance showBalance = new ShowBalance();
+        threadShowBalance = new Thread(showBalance);
+        threadShowBalance.start();
+    }
+
+    public static void consoleReading() {
+        String accountOperation = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            accountOperation = scanner.nextLine().toUpperCase(Locale.ROOT); //чтобы "USD" и "usd" были одинаковыми
+            if (accountOperation.equals("QUIT")) {
+                System.out.println("Работа программы завершена по команде пользователя");
+                threadShowBalance.interrupt();
+                break;
+            } else {
+                addBalance(accountOperation);
+            }
         }
     }
 
+    public static void getUsdCurrency(){
+        usdCurrency.put("HKD", BigDecimal.valueOf(0.13));
+        usdCurrency.put("RMB", BigDecimal.valueOf(0.16));
+    }
 }
