f6de9a2e95e51b91e4fd419862fd7d94a63e0490
KosivchenkoPetr <nightdemon@mail.ru>
2021-09-15 20:33:27 +0300

    test (#8)
    * main code
    
    * Refactoring and tests
    
    * minor changes
    
    * test
    
    * 15092021

---
+++ src/test/java/FormattedBalanceTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormattedBalanceTest {
+
+    @Test
+    void getFormattedBalance() {
+        testWithCurrency();
+        testWithoutCurrency();
+    }
+
+    private void testWithCurrency() {
+
+        String expectedStr = "Остаток в валюте : HKD, сумма: 1000 (USD 130.00)";
+        String balanceStr = new FormattedBalance().getFormattedBalance("HKD", 1000, " (USD 130.00)");
+        assertEquals(expectedStr, balanceStr);
+
+    }
+
+    private void testWithoutCurrency() {
+
+
+        String expectedStr = "Остаток в валюте : USD, сумма: 1000";
+        String balanceStr = new FormattedBalance().getFormattedBalance("USD", 1000, "");
+        assertEquals(expectedStr, balanceStr);
+
+    }
+
+}
